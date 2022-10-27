60f1eacfffe90844a116b293f68c2f30792decd9
Nicole Prim <nicolepprim@gmail.com>
2022-10-27 16:33:43 -0300

    parameter validation

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,13 @@
 const { User } = require("../model/User");
 
 exports.create = async (ctx) => {
-    const {
-      name, age, email, password
-    } = ctx.request.body;
-    console.log(ctx.request.body)
+  const {
+    name, email, age, password
+  } = ctx.request.body;
+
+  if (!name || !email || !age || !password) {
+    throw new Error("Informe todos os parâmetros")
+  }
 
   const user = new User(name, email, age, password);
 
